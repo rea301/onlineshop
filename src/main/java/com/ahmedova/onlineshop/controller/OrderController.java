e36ebd96b1980341af1a30468e15f32d8e8c5ad5
rea301 <ahmedova.renay@gmail.com>
2020-07-12 18:18:38 +0300

    final commit

--- src/main/java/com/ahmedova/onlineshop/controller/OrderController.java
+++ src/main/java/com/ahmedova/onlineshop/controller/OrderController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/contracts")
+@RequestMapping("api/orders")
 public class OrderController {
     @PostMapping
     public void createOrder(@RequestBody Order order){
